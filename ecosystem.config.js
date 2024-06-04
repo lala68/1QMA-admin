229a79b61e0b626f056c47f41063eadb42b5bd39
navid hero <navid_hero_1st@yahoo.com>
2024-06-04 19:21:16 +0330

    add pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  deploy: {
+    production: {
+      user: "root",
+      host: "64.226.74.250",
+      ref: "origin/main",
+      repo: "git@github.com:lala68/1QMA-admin.git",
+      path: "/root/projects/sources/1qma.admin",
+      "pre-deploy": "git reset --hard",
+      "post-deploy":
+        "npm install; pm2 startOrRestart ~/projects/config/1qma.admin.json --update-env --env production; pm2 save;",
+    },
+    staging: {
+      user: "root",
+      host: "64.226.74.250",
+      ref: "origin/dev",
+      repo: "git@github.com:lala68/1QMA-admin.git",
+      path: "/root/projects/sources/staging.1qma.admin",
+      "pre-deploy": "git reset --hard",
+      "post-deploy":
+        "npm install; pm2 startOrRestart ~/projects/config/staging.1qma.admin.json --update-env --env production; pm2 save;",
+    },
+  },
+};
