c6bed1f0b78d1817e4defedbbf63adbad1822c23
ladan <l.fejan@gmail.com>
2024-12-02 14:46:04 +0330

    fix baseUrl

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  // baseUrl : 'https://staging.1qma.games/'
   baseUrl : 'https://api.staging.1qma.games/'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  // baseUrl: 'https://staging.1qma.games/'
   baseUrl: 'https://api.staging.1qma.games/'
 };
